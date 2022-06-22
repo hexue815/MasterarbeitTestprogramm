c2ad34a7a39a2ed39b023373c3e0b6b3697641e4
xuehe <xuehe19900625@gmail.com>
2022-06-22 15:43:24 +0200

    open transaction function

--- NoCacheTest/src/main/java/com/xue/service/UserServiceImpl.java
+++ NoCacheTest/src/main/java/com/xue/service/UserServiceImpl.java
@@ -7,6 +7,7 @@ import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
